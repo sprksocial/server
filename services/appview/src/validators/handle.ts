d26d7d26cfd11683c7c701826330c25f72c86912
Honys <69396539+zHonys@users.noreply.github.com>
2025-02-25 23:46:27 -0300

    Fix build on appview
    remove moderation stuff for now

--- services/appview/src/validators/handle.ts
+++
@@ -1,7 +0,0 @@
-import { t } from "elysia";
-
-export const ValidHandle = t.String({
-	minLength: 3,
-	maxLength: 253,
-	pattern: '^([a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\\.)+[a-zA-Z]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?$'
-});
