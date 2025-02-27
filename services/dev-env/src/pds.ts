0067347fb0054b9b2cf9d293715539b8d6db3b77
daviirodrig <30713947+daviirodrig@users.noreply.github.com>
2025-02-27 19:46:39 -0300

    add dev env and infra from bsky/atproto

---
+++ services/dev-env/src/pds.ts
@@ -0,0 +1,107 @@
+import fs from "node:fs/promises";
+import os from "node:os";
+import path from "node:path";
+import getPort from "get-port";
+import * as ui8 from "uint8arrays";
+import { AtpAgent } from "@atproto/api";
+import { Secp256k1Keypair, randomStr } from "@atproto/crypto";
+import * as pds from "../../pds";
+import { createSecretKeyObject } from "@atproto/pds";
+import { ADMIN_PASSWORD, EXAMPLE_LABELER, JWT_SECRET } from "./const";
+import { PdsConfig } from "./types";
+
+export class TestPds {
+  constructor(
+    public url: string,
+    public port: number,
+    public server: pds.PDS
+  ) {}
+
+  static async create(config: PdsConfig): Promise<TestPds> {
+    const plcRotationKey = await Secp256k1Keypair.create({ exportable: true });
+    const plcRotationPriv = ui8.toString(await plcRotationKey.export(), "hex");
+    const recoveryKey = (await Secp256k1Keypair.create()).did();
+
+    const port = config.port || (await getPort());
+    const url = `http://localhost:${port}`;
+
+    const blobstoreLoc = path.join(os.tmpdir(), randomStr(8, "base32"));
+    const dataDirectory = path.join(os.tmpdir(), randomStr(8, "base32"));
+    await fs.mkdir(dataDirectory, { recursive: true });
+
+    const env: pds.ServerEnvironment = {
+      devMode: true,
+      port,
+      dataDirectory: dataDirectory,
+      blobstoreDiskLocation: blobstoreLoc,
+      recoveryDidKey: recoveryKey,
+      adminPassword: ADMIN_PASSWORD,
+      jwtSecret: JWT_SECRET,
+      serviceHandleDomains: [".test"],
+      bskyAppViewUrl: "https://appview.invalid",
+      bskyAppViewDid: "did:example:invalid",
+      bskyAppViewCdnUrlPattern: "http://cdn.appview.com/%s/%s/%s",
+      modServiceUrl: "https://moderator.invalid",
+      modServiceDid: "did:example:invalid",
+      plcRotationKeyK256PrivateKeyHex: plcRotationPriv,
+      inviteRequired: false,
+      disableSsrfProtection: true,
+      serviceName: "Development PDS",
+      brandColor: "#ffcb1e",
+      errorColor: undefined,
+      logoUrl:
+        "https://uxwing.com/wp-content/themes/uxwing/download/animals-and-birds/bee-icon.png",
+      homeUrl: "https://bsky.social/",
+      termsOfServiceUrl: "https://bsky.social/about/support/tos",
+      privacyPolicyUrl: "https://bsky.social/about/support/privacy-policy",
+      supportUrl: "https://blueskyweb.zendesk.com/hc/en-us",
+      ...config,
+    };
+    const cfg = pds.envToCfg(env);
+    const secrets = pds.envToSecrets(env);
+
+    const server = await pds.PDS.create(cfg, secrets);
+
+    await server.start();
+
+    return new TestPds(url, port, server);
+  }
+
+  get ctx(): pds.AppContext {
+    return this.server.ctx;
+  }
+
+  getClient(): AtpAgent {
+    const agent = new AtpAgent({ service: this.url });
+    agent.configureLabelers([EXAMPLE_LABELER]);
+    return agent;
+  }
+
+  adminAuth(): string {
+    return (
+      "Basic " +
+      ui8.toString(
+        ui8.fromString(`admin:${ADMIN_PASSWORD}`, "utf8"),
+        "base64pad"
+      )
+    );
+  }
+
+  adminAuthHeaders() {
+    return {
+      authorization: this.adminAuth(),
+    };
+  }
+
+  jwtSecretKey() {
+    return createSecretKeyObject(JWT_SECRET);
+  }
+
+  async processAll() {
+    await this.ctx.backgroundQueue.processAll();
+  }
+
+  async close() {
+    await this.server.destroy();
+  }
+}
