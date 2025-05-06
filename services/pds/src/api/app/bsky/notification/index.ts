b053aa58ea893f8ad3ff8d035d5473169e819829
daviirodrig <30713947+daviirodrig@users.noreply.github.com>
2025-05-06 04:34:45 -0300

    remove pds folder

--- services/pds/src/api/app/bsky/notification/index.ts
+++
@@ -1,7 +0,0 @@
-import { AppContext } from '../../../../context'
-import { Server } from '../../../../lexicon'
-import registerPush from './registerPush'
-
-export default function (server: Server, ctx: AppContext) {
-  registerPush(server, ctx)
-}
